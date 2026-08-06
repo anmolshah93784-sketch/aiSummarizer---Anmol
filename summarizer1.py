73795d931bad360c8febe2a812a763a3fa436f00
anmolshah93784-sketch <anmolshah93784@gmail.com>
2026-08-06 08:59:25 +0530

    Update summarizer1.py
    notes summarizer cum answerer

--- summarizer1.py
+++ summarizer1.py
@@ -8,15 +8,8 @@ def summarize_notes(My_notes):
     response = client.chat.completions.create(
         model="Qwen/Qwen2.5-7B-Instruct",
         messages=[
-            {"role": "user", "content": f"You are a professor and you have to summarize the notes in bullet points:\n\n{My_notes}"}
+            {"role": "user", "content": f"You are a professor and you have to summarize the notes in bullet points or give answers if any questions will asked or give solutions if any numericals will asked:\n\n{My_notes}"}
         ],
         max_tokens=500
     )
     return response.choices[0].message.content
-
-if __name__ == '__main__':
-    My_notes = input("Enter your notes: ")
-    print("Hey buddy!\nYour summarised notes are below\nKeep learning...")
-    
-    Result = summarize_notes(My_notes)
-    print(Result)
