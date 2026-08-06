5be0a646f760c8c7be97bc97ef63751c8e2b4004
anmolshah93784-sketch <anmolshah93784@gmail.com>
2026-08-06 09:26:28 +0530

    Update Website.py
    Q/A

--- Website.py
+++ Website.py
@@ -63,7 +63,7 @@ except Exception as e:
 # 3. set (UI)
 st.markdown("<h1 class='main-title'>📚 Anmol AI Notes Summarizer</h1>", unsafe_allow_html=True)
 st.write("<p style='text-align: center; color: #E0E0E0;'>Get instant exam-ready points from your senior AI teacher!</p>", unsafe_allow_html=True)
-
+st.write("<p style='text-align: center; color: #E0E0E0;'>Also get some related exam-ready Q/A.</p>", unsafe_allow_html=True)
 user_notes = st.text_area("📄 Type or paste your long notes below:", height=180, placeholder="Type or paste your content here...")
 
 if st.button("✨ Generate AI Summary", use_container_width=True):
