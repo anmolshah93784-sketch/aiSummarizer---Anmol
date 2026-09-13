6d83d6e67fd93f4f145ea934aba28660eba428bc
anmolshah93784-sketch <anmolshah93784@gmail.com>
2026-09-13 08:31:06 +0530

    Update summarizer1.py
    new model of hugging face

--- summarizer1.py
+++ summarizer1.py
@@ -5,8 +5,8 @@ HF_TOKEN = st.secrets["HF_TOKEN"]
 client = InferenceClient(api_key=HF_TOKEN)
 
 def summarize_notes(My_notes):
-    response = client.chat.completions.create(
-        model="Qwen/Qwen2.5-7B-Instruct",
+    response = client.chat_completion(
+        model="meta-llama/Llama-3.2-3B-Instruct",
         messages=[
             {"role": "user", "content": f"You are a professor and you have to summarize the given notes in bullet points step by step and also give some notes related exam ready questions & answers or solve numericals if asked:\n\n{My_notes}"}
         ],
